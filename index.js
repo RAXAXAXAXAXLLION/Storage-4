54797421641752a6cb26b6b38af8872eabdc5f82
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-30 22:06:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("socket.io").listen(
 require("http").createServer((req,res)=>{
 res.writeHead(200,{"content-type":"text/html"})
-res.write('<script>net=new WebSocket("wss://"+location.hostname+":'+process.env.PORT||3000+'");net.onmessage=(e)=>{eval(e.data)}</script>')
+res.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>')
+res.write('<script>io().on("msg",(e)=>{eval(e.script)})</script>')
 res.end()
 }).listen(process.env.PORT||3000)
-require("nodejs-websocket").createServer((w)=>{
-
-}).listen(process.env.PORT||3000)
+).on("connection",(w)=>{
+w.emit("msg",{script:'alert("Scripting works!")'})
+})
