baa4ff7c08475634109cdaa9e8e4527126fb3055
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-30 16:01:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("http").createServer((req,res)=>{
 res.writeHead(200,{"content-type":"text/html"})
-res.write('<script>net=new WebSocket("wss://"+location.hostname+":8888";net.onmessage=(e)=>{eval(e.data)})</script>')
+res.write('<script>net=new WebSocket("wss://"+location.hostname+":8888");net.onmessage=(e)=>{eval(e.data)})</script>')
 res.end()
 }).listen(process.env.PORT||3000)
 require("nodejs-websocket").createServer((w)=>{
